2184ad1b64b41b710322b523019233a7cada448b
Demakatso <demakatsomay@gmail.com>
2023-05-21 07:53:01 -0700

    shell compilation

--- f2.c
+++ f2.c
@@ -8,17 +8,17 @@
 
 int _putchar(char c)
 {
-        static int i;
-        static char buf[EBSZ];
+static int i;
+static char buf[EBSZ];
 
-        if (c == CBF || i >= EBSZ)
-        {
-                write(1, buf, i);
-                i = 0;
-        }
-        if (c != CBF)
-                buf[i++] = c;
-        return (1);
+if (c == CBF || i >= EBSZ)
+{
+write(1, buf, i);
+i = 0;
+}
+if (c != CBF)
+buf[i++] = c;
+return (1);
 }
 
 /**
@@ -31,35 +31,35 @@ int _putchar(char c)
 
 list *adne(list **head, const char *str, int num)
 {
-        list *new_node, *node;
+list *new_node, *node;
 
-        if (!head)
-                return (NULL);
+if (!head)
+return (NULL);
 
-        node = *head;
-        new_node = malloc(sizeof(list));
-        if (!new_node)
-                return (NULL);
-        _memset((void *)new_node, 0, sizeof(list));
-        new_node->num = num;
-        if (str)
-        {
-                new_node->str = _strdup(str);
-                if (!new_node->str)
-                {
-                        free(new_node);
-                        return (NULL);
-                }
-        }
-        if (node)
-        {
-                while (node->next)
-                        node = node->next;
-                node->next = new_node;
-        }
-        else
-                *head = new_node;
-        return (new_node);
+node = *head;
+new_node = malloc(sizeof(list));
+if (!new_node)
+return (NULL);
+_memset((void *)new_node, 0, sizeof(list));
+new_node->num = num;
+if (str)
+{
+new_node->str = _strdup(str);
+if (!new_node->str)
+{
+free(new_node);
+return (NULL);
+}
+}
+if (node)
+{
+while (node->next)
+node = node->next;
+node->next = new_node;
+}
+else
+*head = new_node;
+return (new_node);
 }
 
 /**
@@ -70,12 +70,12 @@ list *adne(list **head, const char *str, int num)
  */
 char *_strchr(char *s, char c)
 {
-        for( ; *s != '\0'; s++ )
-        {
-                if (*s == c)
-                        return(s);
-        }
-        return (NULL);
+for( ; *s != '\0'; s++ )
+{
+if (*s == c)
+return(s);
+}
+return (NULL);
 }
 
 /**
@@ -87,17 +87,18 @@ char *_strchr(char *s, char c)
 
 int _strcmp(char *s1, char *s2)
 {
-        if (*s1 && *s2)
-        {
-                if (*s1 != *s2)
-                        return (*s1 - *s2);
-                return (_strcmp(s1 + 1, s2 + 1));
-        }
 
-        if (*s1 == *s2)
-                return (0);
-        else
-                return ((*s1 < *s2) ? -1 : 1);
+if (*s1 && *s2)
+{
+if (*s1 != *s2)
+return (*s1 - *s2);
+return (_strcmp(s1 + 1, s2 + 1));
+}
+
+if (*s1 == *s2)
+return (0);
+else
+return ((*s1 < *s2) ? -1 : 1);
 }
 
 /**
@@ -106,13 +107,13 @@ int _strcmp(char *s1, char *s2)
  */
 void _puts(char *str)
 {
-        int i = 0;
+int i = 0;
 
-        if (!str)
-                return;
-        while (str[i] != '\0')
-        {
-                _putchar(str[i]);
-                i++;
-        }
+if (!str)
+return;
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+}
 }
