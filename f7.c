ede035087249b77342fd2cc33a0e6c53178ec925
Demakatso <demakatsomay@gmail.com>
2023-05-21 09:38:29 -0700

    shell complation

--- f7.c
+++ f7.c
@@ -10,13 +10,13 @@
 
 void perr(jobs *job, char *estr)
 {
-        env_put(job->fname);
-        env_put(": ");
-        _decimal(job->line_count, STDERR_FILENO);
-        env_put(": ");
-        env_put(job->argv[0]);
-        env_put(": ");
-        env_put(estr);
+	env_put(job->fname);
+	env_put(": ");
+	_decimal(job->line_count, STDERR_FILENO);
+	env_put(": ");
+	env_put(job->argv[0]);
+	env_put(": ");
+	env_put(estr);
 }
 /**
  * atoi_error -...
@@ -26,24 +26,24 @@ void perr(jobs *job, char *estr)
 
 int atoi_error(char *s)
 {
-        int i = 0;
-        unsigned long int result = 0;
+	int i = 0;
+	unsigned long int result = 0;
 
-        if (*s == '+')
-                s++;
-        for (i = 0;  s[i] != '\0'; i++)
-        {
-                if (s[i] >= '0' && s[i] <= '9')
-                {
-                        result *= 10;
-                        result += (s[i] - '0');
-                        if (result > INT_MAX)
-                                return (-1);
-                }
-                else
-                        return (-1);
-        }
-        return (result);
+	if (*s == '+')
+		s++;
+	for (i = 0;  s[i] != '\0'; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result *= 10;
+			result += (s[i] - '0');
+			if (result > INT_MAX)
+				return (-1);
+		}
+		else
+			return (-1);
+	}
+	return (result);
 }
 
 /**
@@ -55,21 +55,21 @@ int atoi_error(char *s)
  */
 char *changeInt(long int num, int base, int flags)
 {
-        static char buffer[50];
-        char *p = buffer + sizeof(buffer) - 1;
-        unsigned long n = num < 0 && !(flags & BLT_UNSC) ? -num : num;
-        char sign = num < 0 && !(flags & BLT_UNSC) ? '-' : 0;
-        char *chain = flags & BLT_CLC ? "0123456789abcdef" : "0123456789ABCDEF";
+	static char buffer[50];
+	char *p = buffer + sizeof(buffer) - 1;
+	unsigned long n = num < 0 && !(flags & BLT_UNSC) ? -num : num;
+	char sign = num < 0 && !(flags & BLT_UNSC) ? '-' : 0;
+	char *chain = flags & BLT_CLC ? "0123456789abcdef" : "0123456789ABCDEF";
 
-        *p = '\0';
-        while (n != 0)
-        {
-                *--p = chain[n % base];
-                n /= base;
-        }
-        if (sign)
-                *--p = sign;
-        return (p);
+	*p = '\0';
+	while (n != 0)
+	{
+		*--p = chain[n % base];
+		n /= base;
+	}
+	if (sign)
+		*--p = sign;
+	return (p);
 }
 
 /**
@@ -79,7 +79,7 @@ char *changeInt(long int num, int base, int flags)
 
 void no_hashtags(char *buf)
 {
-        char *hashtag = _strchr(buf, '#');
-        if (hashtag != NULL && (hashtag == buf || *(hashtag - 1) == ' '))
-                *hashtag = '\0';
+	char *hashtag = _strchr(buf, '#');
+	if (hashtag != NULL && (hashtag == buf || *(hashtag - 1) == ' '))
+		*hashtag = '\0';
 }
